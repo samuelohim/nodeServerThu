28f0c084f3e0f720fd17f5a8ec7423f3b2b6897c
Samuel Y <samuel.y@weimeit.com>
2017-02-23 11:42:07 +0800

    controller successfully implemented for server.js

---
+++ app/controllers/main.controller.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+	showHome: (req, res)=>{
+
+		res.send('Hi from main.controller.js');
+
+	}
+
+};
--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,7 @@
 const oe = require('express')
 router = oe.Router();
+mainController = require('./controllers/main.controller');
+
 module.exports = router;
 
-router.get('/', (req, res)=>{
-
-	res.send('Hi from router.js');
-})
+router.get('/', mainController.showHome);
